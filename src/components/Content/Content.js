2743d9b48128993b48e2dbb29294255785af78ea
Kuzin Igor <igor1utsu@gmail.com>
2022-11-04 16:07:19 +0300

    Написал логику поиска

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { useEffect, useState } from "react"
+import { useEffect, useState, useMemo } from "react"
 import "./Content.scss"
 import Post from "./Post/Post"
 import PostForm from "./PostForm/PostForm"
@@ -20,7 +20,16 @@ export default () => {
   const params = useParams()
   const isLoggedIn = useSelector(selectIsLoggedin)    // извлекаем состояние авторизации из Redux
   const dispath = useDispatch()
+  const [searchData, setSearchData] = useState('')
+  // если есть данные для поиска фильтруем массив на совпадение в title и description
+  const searchResult = useMemo(() => 
+    searchData ? postList.filter(post => post.title.toUpperCase().includes(searchData.toUpperCase()) || post.description.toUpperCase().includes(searchData.toUpperCase())) : null, [postList, searchData])
 
+  const handleSearch = (e) => {
+    setSearchData(e.target.value)   // изменяем данные для поиска
+  }
+  
+  console.log(['find:', searchData, 'result:', searchData])
 
   useEffect(() => {
     dispath(loadPosts())      // загружаем посты
@@ -34,10 +43,8 @@ export default () => {
         <div className="content__header">
           <h2 className="title">{isFavourites ? 'Favourites' : 'My blog :)'}</h2>
           <div className="search-wrapper">
-            <input className="search-input" type="text" placeholder="поиск"></input>
-            <button className="btn--icon" onClick={() => alert('пока что не работает :(')}>
-              <SearchIcon className="icon"/>
-            </button>
+            <input className="search-input" type="text" placeholder="поиск" value={searchData} onChange={handleSearch}></input>
+            <SearchIcon className="icon"/>
           </div>
         </div>
 
@@ -46,7 +53,10 @@ export default () => {
             (postList.length === 0) && <h4 className="content__message">Постов нет</h4>
           }
           {
-            postList.map((post, index) => {
+            searchResult && <h4 className="content__message">Найдено: {searchResult.length}</h4>
+          }
+          {
+            !searchResult && postList.map((post, index) => {
               return (
                 <Post
                   post={post}
@@ -57,7 +67,19 @@ export default () => {
               )
             })
           }
-        {isLoggedIn && !isFavourites && <button className="btn" onClick={() => setIsVisibleForm(true)}>Добавить пост</button>}      {/* если авторизованны и не в избранном: показываем кнопку */}
+          {
+            searchResult && searchResult.map((post, index) => {
+              return (
+                <Post
+                  post={post}
+                  key={post.id}
+                  setSelectPost={setSelectPost}
+                  setIsVisibleForm={setIsVisibleForm}
+                />
+              )
+            })
+          }
+        {isLoggedIn && !isFavourites && !searchResult && <button className="btn" onClick={() => setIsVisibleForm(true)}>Добавить пост</button>}      {/* если авторизованны и не в избранном: показываем кнопку */}
         </div>
         
         
