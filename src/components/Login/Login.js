640dc48d3e4fb605a4e8a3b1ae2ff8e8f50876e5
Kuzin Igor <igor1utsu@gmail.com>
2022-11-11 02:17:43 +0300

    Добавил авторизацию

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,33 +1,35 @@
-import React, { useRef } from "react"
+import React from "react"
 import { useState } from "react"
 import "./Login.scss"
 import { ReactComponent as Logo } from "../../assets/svg/logo.svg"
 import { useDispatch } from "react-redux"
 import { logIn } from "../../store/slices/auth"
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({loginName, setLoginName }) => {
-    let [name, setName] = useState(loginName)
+    let [email, setEmail] = useState('')
+    let [password, setPassword] = useState('')
     const dispatch = useDispatch()
-
-    // Создаем ключ: Логин, Пароль
-    const loginRef = useRef()
-    const passwordRef = useRef()
-
+    const history = useHistory()
+    
     const submit = (e) => {
         e.preventDefault()
-        // Считываем значение ключей из imput
-        const userData = {
-            login: loginRef.current.value,
-            password: passwordRef.current.value
-        }
-        
-        setLoginName(userData.login)            // заносим login в стейт
-        dispatch(logIn())                       // вызываем функцию авторизации из хранилища Redux
-        localStorage.setItem('login', name)     // заносим login в локальное хранилище
-        window.location.href = '/blog'          // переходим на главную страницу
+
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                dispatch(logIn())                       // вызываем функцию авторизации из хранилища Redux
+                history.push('./blog')                  // переходим на главную страницу
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
     }
 
 
@@ -38,17 +40,17 @@ export default ({loginName, setLoginName }) => {
                     <Logo className="logo__icon"/>
                 </div>
                 <input 
-                    className="input" 
-                    type="text" ref={loginRef} 
-                    value={name} 
-                    onChange={event => setName(event.target.value)} 
-                    placeholder="name"
+                    className="input"
+                    value={email}
+                    onChange={event => setEmail(event.target.value)} 
+                    placeholder="email"
                     required
                 ></input>
                 <input 
                     className="input" 
                     type="password" 
-                    ref={passwordRef} 
+                    value={password}
+                    onChange={event => setPassword(event.target.value)} 
                     placeholder="password"
                     required
                 ></input>
