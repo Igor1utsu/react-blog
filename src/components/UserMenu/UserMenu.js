acd03272b1bb833578298fce84dce44313613ce9
Kuzin Igor <igor1utsu@gmail.com>
2022-11-18 02:27:14 +0300

    Изменил стили UserMenu

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { getAuth, signOut } from "firebase/auth"
 import { useHistory } from "react-router-dom"
+import imgContacts from '../../assets/contacts.png'
 import "./UserMenu.scss"
 
 export default ({ isVisibleUserMenu }) => {
@@ -14,10 +15,12 @@ export default ({ isVisibleUserMenu }) => {
     }
 
     return (
-        <div className={ isVisibleUserMenu ? 'user-menu user-menu-active' : 'user-menu' }>
-            <div className="user-menu__avatar"></div>
+        <nav className={ isVisibleUserMenu ? 'user-menu user-menu-active' : 'user-menu' }>
+            <div className="user-menu__avatar">
+                <img src={imgContacts}/>
+            </div>
             <div className="user-menu__link">Язык</div>
-            <div className="user-menu__link user-menu__link--logout" onClick={(e) => clickLogOut(e)}>Выход</div>
-        </div>
+            <div className="user-menu__link" onClick={(e) => clickLogOut(e)}>Выход</div>
+        </nav>
     )
 }
