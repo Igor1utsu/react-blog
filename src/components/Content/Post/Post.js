4154c1ba0c0923f4f42d8adcb1c0f14138cb0c2d
Kuzin Igor <igor1utsu@gmail.com>
2022-11-23 03:48:25 +0300

    убрал ссылку на пост в Bookmarks

--- src/components/Content/Post/Post.js
+++ src/components/Content/Post/Post.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 import imgPlaceholder from '../../../assets/place-holder-img.png'
 import { HeartIcon } from '../../HeartIcon/HeartIcon'
-import { ReactComponent as TrashIcon } from "../../../assets/svg/trash.svg"
 import "./Post.scss"
 import { useHistory, useLocation } from 'react-router-dom'
 import { getIsLoggedIn } from '../../../store/slices/auth'
@@ -11,11 +10,13 @@ import { showDeleteConfirm } from '../../../utils/modals.utils'
 import DropDownPost from '../../DropdownPost/DropDownPost'
 import { getBookmarks } from '../../../store/reducers/bookmarksReducer'
 import { useMemo } from 'react'
+import { useFavourites } from '../../../utils/hooks'
 
 
 
 export default ({ post, setSelectPost, setIsVisibleForm }) => {
         const { id, title, description, thumbnail, liked } = post
+        const { isBookmarks } = useFavourites()                            // загружаем функционал для избранного
         const bookmarkList = useSelector(getBookmarks)                        // извлекаем список закладок из Redux
         const bookmark = useMemo(() => bookmarkList.find(bookmark => bookmark.postID === post.id), [bookmarkList, post.id])   // есть пост в закладках?
         const history = useHistory()
@@ -48,7 +49,7 @@ export default ({ post, setSelectPost, setIsVisibleForm }) => {
                       <h3 className="post__title">{title}</h3>
                       <DropDownPost id={id} bookmark={bookmark} handleEditPost={handleEditPost} handleDeletePost={handleDeletePost}/>
                     </div>
-                    <div className="post__description" onClick={() => history.push(`${location.pathname}/${id}`)}>{description}</div>
+                    <div className="post__description" onClick={!isBookmarks ? () => history.push(`${location.pathname}/${id}`) : null}>{description}</div>
                     <nav className="application" onClick={(e) => {e.stopPropagation()}}>
                         <button className="btn--application btn--application-liks" onClick={isLoggedIn ? (e) => handleLikePost(e) : () => history.push('/login')}>
                           <HeartIcon liked={liked} id={id}/>
